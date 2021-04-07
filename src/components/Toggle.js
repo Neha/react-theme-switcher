eaf31c36e3617244f9511abd37a29a8cbc92b675
Lakshya Thakur <lapstjup@gmail.com>
2021-04-07 23:19:28 +0530

    Remove PropTypes from Toggle component

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,14 +1,8 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { useTheme } from "./Context";
 
 const Toggle = () => {
   const { theme, handleTheme } = useTheme();
   return <button onClick={handleTheme}>{theme}</button>;
 };
-
-Toggle.propTypes = {
-  text: PropTypes.string,
-};
-
 export default Toggle;
